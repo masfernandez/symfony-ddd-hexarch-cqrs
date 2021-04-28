8a34a1c62743654425cd5b9d25f20293bf10b600
Miguel Ángel Sánchez Fernández <mangel.sanfer@gmail.com>
2021-04-28 20:21:02 +0200

    [0.3.6] - 2021-04-28
    See CHANGELOG file to see all details of this version at:
    
    https://github.com/masfernandez/symfony-ddd-hexarch-cqrs/blob/master/CHANGELOG.md
    
    And to see all code changes at:
    
    https://github.com/masfernandez/symfony-ddd-hexarch-cqrs/compare/v0.3.5...v0.3.6

--- src/Shared/Infrastructure/Symfony/AuthenticatorSubscriber.php
+++ src/Shared/Infrastructure/Symfony/AuthenticatorSubscriber.php
@@ -7,6 +7,8 @@ namespace Masfernandez\Shared\Infrastructure\Symfony;
 use Masfernandez\MusicLabel\Auth\Application\Jwt\Authenticate\AuthenticateJwTokenCommand;
 use Masfernandez\MusicLabel\Auth\Application\Token\Authenticate\AuthenticateTokenCommand;
 use Masfernandez\MusicLabel\Auth\Domain\Model\JsonWebToken\JwTokenValue;
+use Masfernandez\MusicLabel\Auth\Domain\Model\Token\TokenValue;
+use Masfernandez\MusicLabel\Shared\Infrastructure\InputRequest\BadRequest;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +17,8 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validation;
 
 final class AuthenticatorSubscriber implements EventSubscriberInterface
 {
@@ -34,6 +38,9 @@ final class AuthenticatorSubscriber implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * @throws BadRequest
+     */
     public function onKernelRequest(RequestEvent $event): void
     {
         if ($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
@@ -50,19 +57,41 @@ final class AuthenticatorSubscriber implements EventSubscriberInterface
         $signature = $request->cookies->get('signature') ?? false;
         $token = str_replace('Bearer ', '', $token);
         if ($this->isHeaderAndPayPattern($token) && $signature !== false) {
+            // validate input
+            $violations = Validation::createValidator()->validate(
+                ['token' => $token],
+                new Assert\Collection(['token' => JwTokenValue::getConstraints()])
+            );
+
+            if ($violations->count() > 0) {
+                return;
+            }
+
             $command = new AuthenticateJwTokenCommand($token . '.' . $signature);
         } else {
+            // validate input
+            $violations = Validation::createValidator()->validate(
+                ['token' => $token],
+                new Assert\Collection(['token' => TokenValue::getConstraints()])
+            );
+
+            if ($violations->count() > 0) {
+                return;
+            }
+
             $command = new AuthenticateTokenCommand($token);
         }
 
         try {
             $this->commandBus->dispatch($command);
         } catch (HandlerFailedException) {
-            $event->setResponse(new JsonResponse(
-            //@todo set json api message here
-                ['error' => 'Invalid credentials'],
-                Response::HTTP_FORBIDDEN
-            ));
+            $event->setResponse(
+                new JsonResponse(
+                //@todo set json api message here
+                    ['error' => 'Invalid credentials'],
+                    Response::HTTP_FORBIDDEN
+                )
+            );
         }
     }
 
